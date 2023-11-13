d2a776ae67917fccc1ae9ada8b62497b115471a4
Lenny Chen <lenny.chen@spectrocloud.com>
2023-11-13 10:40:40 -0800

    fix: added script to check for and delete packer cache

---
+++ edge/vmware/clone_vm_template/delete-packer-cache.sh
@@ -0,0 +1,20 @@
+#!/bin/bas
+
+source /edge/vmware/clone_vm_template/setenv.sh
+
+# Check if the packer cache exists
+CACHE_EXISTS=$(govc datastore.ls -ds=$vcenter_datastore /packer_cache 2>/dev/null)
+
+if [ -z "$CACHE_EXISTS" ]; then
+  echo "Existing packer cache not found. Nothing to delete."
+else
+  # Cache exists, attempt to delete it
+  govc datastore.rm -ds=$vcenter_datastore /packer_cache
+  RESULT=$?
+  if [ $RESULT -eq 0 ]; then
+    echo "Deleted previous packer cache."
+  else
+    echo "Failed to delete packer cache."
+  fi
+fi
+
