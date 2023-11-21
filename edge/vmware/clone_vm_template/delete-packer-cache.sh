d857c12ba858f2358c184bfb411cc46520addb6b
Lenny Chen <55669665+lennessyy@users.noreply.github.com>
2023-11-21 13:46:40 -0800

    fix: added packer plugin section & improved error handling logic (#26)
    * fix: added packer plugin section & improved error handling logic
    
    * chore: modify script logic to rule out other errors
    
    * chore: make error message less specific as we do not know the actual error
    
    * chore: add logic for error handling
    
    * chore: redirect successul standard output of first command to null
    
    ---------
    
    Co-authored-by: Lenny Chen <lenny.chen@spectrocloud.com>

--- edge/vmware/clone_vm_template/delete-packer-cache.sh
+++ edge/vmware/clone_vm_template/delete-packer-cache.sh
@@ -2,21 +2,27 @@
 
 source /edge/vmware/clone_vm_template/setenv.sh
 
-# Check if the packer cache exists
-CACHE_EXISTS=$(govc datastore.ls -ds=$vcenter_datastore /packer_cache 2>&1)
-
-if [ -z "$CACHE_EXISTS" ]; then
-  echo "Existing packer cache not found. Nothing to delete."
-else
-  # Cache exists, attempt to delete it
-  govc datastore.rm -ds=$vcenter_datastore /packer_cache
-  RESULT=$?
-  if [ $RESULT -eq 0 ]; then
-    echo "Deleted previous packer cache."
-    exit 0
-  else
-    echo "Failed to delete packer cache."
+# First, list directories to confirm access and credentials
+govc datastore.ls -ds=$vcenter_datastore > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Error accessing datastore."
     exit 1
-  fi
 fi
 
+# Now, try to list the packer_cache directory
+PACKER_CACHE_OUTPUT=$(govc datastore.ls -ds=$vcenter_datastore /packer_cache 2>&1)
+PACKER_CACHE_STATUS=$?
+
+if [ $PACKER_CACHE_STATUS -ne 0 ]; then
+    echo "Existing packer cache not found. Nothing to delete."
+else
+    # Cache exists, attempt to delete it
+    govc datastore.rm -ds=$vcenter_datastore /packer_cache
+    if [ $? -eq 0 ]; then
+        echo "Deleted previous packer cache."
+        exit 0
+    else
+        echo "Failed to delete packer cache."
+        exit 1
+    fi
+fi
